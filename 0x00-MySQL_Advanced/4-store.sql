7d2d1931f5cb52557a1993422d088c5601493c55
Aymen Hommani <crosspieloo@gmail.com>
2025-05-02 05:03:28 +0100

    adding two more tasks that are finished

---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,14 @@
+-- Trigger to decrease item quantity when a new order is insterted
+
+DELIMITER $$
+
+CREATE TRIGGER decrease_quanitity_after_order
+AFTER INSERT ON orders
+FOR EACH ROW
+BEGIN
+    UPDATE items
+    SET quantity = quantity - NEW.number
+    WHERE name = NEW.item_name
+END$$
+
+DELIMITER ;
