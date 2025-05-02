eb9f2fa3b9a15321723b3dd2ad87be11c4ee74de
Aymen Hommani <crosspieloo@gmail.com>
2025-05-02 04:12:02 +0100

    adding the finished tasks for today

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- creates a users table with unique email and safe for re-execution
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
---
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -0,0 +1,11 @@
+-- a sql script that ranks country origins of bands by total sum of fans
+
+SELECT
+    origin,
+    SUM(fans) AS nb_fans
+FROM
+    metal_bands
+GROUP BY
+    origin
+ORDER BY
+    nb_fans DESC;
